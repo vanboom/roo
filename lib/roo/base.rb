82f57dc207c962cf02c93bce4c20e2f236cc8092
Vanboom <dvboom@hotmail.com>
2018-05-16 17:53:09 -0500

    allow missing columns #421

--- lib/roo/base.rb
+++ lib/roo/base.rb
@@ -279,7 +279,6 @@ class Roo::Base
 
   def each(options = {})
     return to_enum(:each, options) unless block_given?
-
     if options.empty?
       1.upto(last_row) do |line|
         yield row(line)
@@ -312,7 +311,8 @@ class Roo::Base
     each do |row|
       line_no += 1
       headers = query.map { |q| row.grep(q)[0] }.compact
-      if headers.length == query.length
+      # #421 allow missing header columns
+      if headers.length > 1
         @header_line = line_no
         return return_headers ? headers : line_no
       else
@@ -495,13 +495,17 @@ class Roo::Base
   end
 
   def set_headers(hash = {})
+    # hash contains the header search and column name map
+
     # try to find header row with all values or give an error
     # then create new hash by indexing strings and keeping integers for header array
-    header_row = row_with(hash.values, true)
-    @headers = {}
-    hash.each_with_index do |(key, _), index|
-      @headers[key] = header_index(header_row[index])
-    end
+
+    # @headers contains the header row from the spreadsheet
+    @headers = row_with(hash.values, true)
+
+    # REJECT COLUMN NAMES FROM HASH THAT DO NOT EXIST, #421
+    hash.reject!{ |k,v| @headers.grep(v).empty?}
+    @headers = Hash[hash.keys.zip(@headers.map { |x| header_index(x) })]
   end
 
   def header_index(query)
--- roo.gemspec
+++ roo.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version  = ">= 2.5.0"
 
   spec.add_dependency 'nokogiri', '~> 1'
-  spec.add_dependency 'rubyzip', '>= 1.3.0', '< 3.0.0'
+  spec.add_dependency 'rubyzip', '>= 1.1.0'
 
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'minitest', '~> 5.4', '>= 5.4.3'
